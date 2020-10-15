cd49b9725c95e2c72b28abf66fc02da78055428b
Noel <noelluna30@gmail.com>
2020-10-15 18:57:57 -0500

    Adding files

--- PyPoll.py
+++ PyPoll.py
@@ -13,6 +13,15 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
 
+#contains candidates
+candidate_options = []
+
+#votes per candidate
+candidate_votes = {}
+
+#contains total votes
+total_votes = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -22,11 +31,32 @@ with open(file_to_load) as election_data:
     # Print the header row.
     headers = next(file_reader)
     print(headers)
-    
-    # # Print each row in the CSV file.
-    # for row in file_reader:
-    #     print(row[0])
 
+    # Print each row in the CSV file.
+    for row in file_reader:
+
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+            candidate_options.append(candidate_name)
+
+            # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+    for candidate in candidate_options:
+        votes = candidate_votes[candidate]
+
+        vote_percentage = float(votes)/float(total_votes) * 100
+
+        print(candidate + " recieved " + str(format(vote_percentage, '.2f')) + "% of the vote.")
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
