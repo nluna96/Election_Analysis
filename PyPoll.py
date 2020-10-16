ad7a80d560664280c5611c4588b3a643fce11608
Noel <noelluna30@gmail.com>
2020-10-16 18:27:33 -0500

    Found Candidate Winnner

--- PyPoll.py
+++ PyPoll.py
@@ -16,11 +16,16 @@ file_to_save = os.path.join("Analysis", "election_analysis.txt")
 #contains candidates
 candidate_options = []
 
-#votes per candidate
+#votes per candidate 
 candidate_votes = {}
 
 #contains total votes
 total_votes = 0
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -30,7 +35,7 @@ with open(file_to_load) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
+    print(str(headers) + "\n")
 
     # Print each row in the CSV file.
     for row in file_reader:
@@ -52,11 +57,33 @@ with open(file_to_load) as election_data:
         candidate_votes[candidate_name] += 1
 
     for candidate in candidate_options:
+
         votes = candidate_votes[candidate]
 
         vote_percentage = float(votes)/float(total_votes) * 100
 
-        print(candidate + " recieved " + str(format(vote_percentage, '.2f')) + "% of the vote.")
+        print(candidate + " " + str(format(vote_percentage, '.1f')) + "% (" + str(votes) + ")\n")
+         
+        # Determine winning vote count and candidate
+        # 1. Determine if the votes are greater than the winning count.
+        if votes > winning_count:
+            # 2. If true then set winning_count = votes and winning_percent =
+            # vote_percentage.
+
+            winning_count = votes
+            winning_percentage = vote_percentage
+
+            # 3. Set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate
+
+
+    print("-------------------------------\n")
+    print("Winner: " +  str(winning_candidate) + "\n")
+    print("Winning Vote Count: " +  str(winning_count) + "\n")
+    print("Winning Percentage: " +  str(winning_percentage) + "\n")
+    print("-------------------------------\n")
+        
+
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
